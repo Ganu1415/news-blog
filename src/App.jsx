684d4f2c5b75693c5190598701ad963b0e417605
Ganesh Vhanmane <mr.vga1415@gmail.com>
2024-07-21 21:22:38 +0530

    add some pakages

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
-import React from 'react';
-import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import BlogPostList from './components/BlogPostList';
-import BlogPostDetails from './components/BlogPostDetails';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import BlogPostList from "./components/BlogPostList";
+import BlogPostDetails from "./components/BlogPostDetails";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1976d2",
+    },
+  },
+});
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<BlogPostList/>} />
-        <Route path="/post/:title" element={<BlogPostDetails />} />
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<BlogPostList />} />
+          <Route path="/post/:title" element={<BlogPostDetails />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 };
 
